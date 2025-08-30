56dc474397cde616f13b4559820b359bb5efb594
Jhonel12 <jhonelmira@gmail.com>
2025-08-30 10:15:08 +0800

    deploy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Github, Linkedin, Mail, Twitter } from "lucide-react";
+import { Facebook, Github, Linkedin, Mail } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
